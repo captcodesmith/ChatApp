fe0947812bd714144db7e7c7fddc44e421d74ba2
captcodesmith <naman.personal.sharma@gmail.com>
2023-05-16 00:51:27 +0530

    feat: FE Changes completed

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
-import cors from 'cors';
+import cors from 'cors'; //api reqs to happen from any origin
+import axios from 'axios';
 
 const app = express();
 app.use(json());
@@ -7,7 +8,16 @@ app.use(cors({ origin: true }));
 
 app.post('/authenticate', async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: 'sha256...' });
+  try {
+    const r = await axios.post(
+      'https://api.chatengine.io/users/',
+      { username: username, secret: username, first_name: username },
+      { headers: { 'private-key': '85d3f70f-6e34-488f-9353-ebd8cee53cbb' } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
 app.listen(3001, () => {
---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,6 @@
+const ChatsPage = (props) => {
+  console.log(props);
+  return <div>Chats... {props.user.username} </div>;
+};
+
+export default ChatsPage;
